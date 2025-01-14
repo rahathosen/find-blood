da869fdbd37f489b2421879963e41408d9db8fa9
rahathosen <rahathosen.me@gmail.com>
2025-01-14 15:02:40 +0600

    feat: add Twitter image CDN to Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "cdn.pixabay.com",
       },
+      {
+        protocol: "https",
+        hostname: "pbs.twimg.com",
+      },
     ],
   },
 };
